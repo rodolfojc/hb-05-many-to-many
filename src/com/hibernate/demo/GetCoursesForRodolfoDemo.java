7763233f9516284a717b0395358648196159c51b
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-02-15 22:32:32 +0000

    App Test Three
    Demo

---
+++ src/com/hibernate/demo/GetCoursesForRodolfoDemo.java
@@ -0,0 +1,58 @@
+package com.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.hibernate.demo.entity.Course;
+import com.hibernate.demo.entity.Instructor;
+import com.hibernate.demo.entity.InstructorDetail;
+import com.hibernate.demo.entity.Review;
+import com.hibernate.demo.entity.Student;
+
+public class GetCoursesForRodolfoDemo {
+
+	public static void main(String[] args) {
+		
+		//CREATE SESSION FACTORY
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.addAnnotatedClass(Course.class)
+								.addAnnotatedClass(Review.class)
+								.addAnnotatedClass(Student.class)
+								.buildSessionFactory();
+		
+		//CREATE SESSION
+		Session session = factory.getCurrentSession();
+		
+		try {
+			
+			//START A TRANSACTION
+			session.beginTransaction();
+			
+			//GET RODOLFO FROM THE DATABASE
+			int studentId = 1;
+			
+			Student tempStudent = session.get(Student.class, studentId);
+			System.out.println("\nLoading courses...");
+			System.out.println("Courses: "+tempStudent.getCourses());
+			
+			//COMMIT TRANSACTION
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+			
+		}
+		finally {
+			//ADD CLEAN UP CODE
+			session.close();
+			
+			factory.close();
+		}
+		
+		
+	}
+
+}
