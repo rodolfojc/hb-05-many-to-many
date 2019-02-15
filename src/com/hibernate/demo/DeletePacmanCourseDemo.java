597dd37d356cf8be84a4956f457cf7fe5c3c648d
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-02-15 22:37:17 +0000

    App Test Four - Delete
    Demo

---
+++ src/com/hibernate/demo/DeletePacmanCourseDemo.java
@@ -0,0 +1,61 @@
+package com.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.hibernate.demo.entity.Course;
+import com.hibernate.demo.entity.Instructor;
+import com.hibernate.demo.entity.InstructorDetail;
+import com.hibernate.demo.entity.Review;
+import com.hibernate.demo.entity.Student;
+
+public class DeletePacmanCourseDemo {
+
+	public static void main(String[] args) {
+		
+		//CREATE SESSION FACTORY
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.addAnnotatedClass(Course.class)
+								.addAnnotatedClass(Review.class)
+								.addAnnotatedClass(Student.class)
+								.buildSessionFactory();
+		
+		//CREATE SESSION
+		Session session = factory.getCurrentSession();
+		
+		try {
+			
+			//START A TRANSACTION
+			session.beginTransaction();
+			
+			//GET RODOLFO FROM THE DATABASE
+			int courseID = 10;
+			
+			Course tempCourse = session.get(Course.class, courseID);
+			System.out.println("\nLoading courses...");
+			System.out.println("Deleting course: "+tempCourse);
+			
+			//DELETE COURSE
+			session.delete(tempCourse);
+			
+			//COMMIT TRANSACTION
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+			
+		}
+		finally {
+			//ADD CLEAN UP CODE
+			session.close();
+			
+			factory.close();
+		}
+		
+		
+	}
+
+}
